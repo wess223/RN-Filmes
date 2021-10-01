29f584b92e29a937bf0334f2f4961bee68a88232
Wesley Alves <wesley.info07@gmail.com>
2021-10-01 16:32:08 -0300

    init

---
+++ app-filmes/src/pages/Movies/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+function Movies(){
+    return (
+        <View>
+            <Text>tela meus filmes</Text>
+        </View>
+    );
+}
+
+export default Movies;
